549b6d3f30c7a51419737425e7cdc8be319557c5
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-04 21:13:15 +0200

    edits

--- components/IntroSection/IntroSection.js
+++ components/IntroSection/IntroSection.js
@@ -4,7 +4,7 @@ const IntroSection = () => {
   return (
     <section className={styles.introSection}>
       <h1 className={styles.introSection__title}>Hello, my name is Ahmed.</h1>
-      <p className={styles.introSection__description}>I am a MERN developer.</p>
+      <p className={styles.introSection__description}>I am a Software Engineer.</p>
     </section>
   );
 };
