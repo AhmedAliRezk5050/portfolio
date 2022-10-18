0ba8e574ad8a7d573bbd1c02ce265023fe953a71
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-19 01:00:11 +0200

    edits

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,15 @@ import Head from 'next/head';
 import ConnectSection from '../components/ConnectSection/ConnectSection';
 import IntroSection from '../components/IntroSection/IntroSection';
 import ProjectsSection from '../components/ProjectsSection/ProjectsSection';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Ahmed Ali - MERN developer</title>
+        <title>Ahmed Ali - Software Engineer</title>
         <meta
           name='description'
-          content='Ahmed Ali - MERN developer Portfolio'
+          content='Ahmed Ali - Software Engineer Portfolio'
         />
       </Head>
       <IntroSection />
